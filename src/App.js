e85e731b2e1f1f504962e2c56b62ea918d0422b1
Jiseok Son <jiseokson@gmail.com>
2023-08-24 15:28:32 +0900

    add: fetch post

--- src/App.js
+++ src/App.js
@@ -1,27 +1,42 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { useState, useEffect } from "react";
+
+function Post() {
+  const [post, setPost] = useState({});
+  const [isLading, setLoading] = useState(true);
+
+  const getPost = async () => {
+    const response = await fetch("http://api.igoofficial.com/posts/");
+    const json = await response.json();
+    setPost(json);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    getPost();
+  }, []);
+
+  return (
+    <div>
+      {isLading ? (
+        <h1>loading...</h1>
+      ) : (
+        <div>
+          <h1>post.title</h1>
+          <p>post.content</p>
+        </div>
+      )}
+    </div>
+  );
+}
 
 function App() {
   return (
     <div>
       <h1>Hello this is my site!</h1>
+      <Post />
     </div>
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
   );
 }
 
